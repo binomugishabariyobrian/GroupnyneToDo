db6cd74ef9f0202a650bf64ed195f0581af3e8a0
Ssekyanzi-Joel <ssekyanzijoel0@gmail.com>
2023-03-17 18:26:49 +0300

    models to the main branch

--- to_do_app/PNYNE/models.py
+++ to_do_app/PNYNE/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Task(models.Model):
+    user = models.Foreignkey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.Charfield(max_legnth=200)
+    description = models.TextField(null=True, blank=True)
+    complete = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add = True)
+    due_date = models.DateTimeField(null=True, blank=True) #the field can be empty.
+    priority = models.CharField(max_length=20, null=True, blank=True)#the field can be empty.
+    completed_date = models.DateTimeField(null=True, blank=True)#the field can be empty.
+    tags = models.ManyToManyField('Tag', blank=True)#tasks can be associated with multiple tags
+
+
+
+    def _str_(self):#human readable string representation of task
+        return self.title
+    class Meta:
+        order_with_respect_to = 'user'#tasks should be ordered based on their associsted user
+
+    def mark_as_complete(self):
+        self.complete = True
+        self.completed_date = datetime.now()
+        self.save()
+
+    def mark_as_incomplete(self):
+        self.complete = False
+        self.completed_date = None
+        self.save()
+
+    @staticmethod
+    def get_all_completed_tasks():
+        return Task.objects.filter(complete=True)
+
+    @staticmethod
+    def get_tasks_due_today():
+        return Task.objects.filter(due_date=date.today())
+
+class Tag(models.Model):    #Tag model that represents a tag that can be associated with tasks
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+
+
+
+
+
