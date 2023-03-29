3583419e64ec8408b7728d47b2c542dcd31ae0d4
BukenyaAbdusalaam <bukenyaabdusalaam508@gmail.com>
2023-03-29 22:01:12 +0300

    Salaam

--- to_do_app/PNYNE/views.py
+++ to_do_app/PNYNE/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
 
-# Create your views here.
+
+def homepage(request):
+    return render(request, 'app_pages/index.html')
+
+
+def signin(request):
+    return render(request, 'accounts/signin.html')
+
+
+def signup(request):
+    return render(request, 'accounts/register.html')
+
+
+def signout(request):
+    logout(request)
+    return redirect('/')
